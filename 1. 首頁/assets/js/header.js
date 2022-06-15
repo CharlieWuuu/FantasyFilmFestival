750b3ddb77782f864fd2a8ee528bbd435ce5637a
CL Wu <CharlieWu500@gmail.com>
2022-06-15 18:20:44 +0800

    push

--- 1. 首頁/assets/js/header.js
+++ 1. 首頁/assets/js/header.js
@@ -3,34 +3,39 @@ function myFunction(e) {
   // 減去一半的螢幕寬/高，讓滑鼠在中間時，接近原本的版面配置
   var x = e.pageX - screen.width * 0.5;
   var y = e.pageY - screen.height * 0.5;
+  $('.header__bg__container')
+    .children()
+    .children()
+    .css('transition-duration', '1s');
+  $('.header__main__container')
+    .children()
+    .children()
+    .css(
+      { 'transition-duration': '0.5s' },
+      { 'transition-timing-function': 'ease' },
+    );
 
   // 背景大尾巴
-
   $('#tailBig').css('top', 'calc( -10vh + ' + -0.02 * y + 'px )');
   $('#tailBig').css('left', 'calc( 15% + ' + -0.02 * x + 'px)');
 
   // 背景小尾巴(左)
-
   $('#tailSmall1').css('bottom', 'calc( 0vh + ' + -0.01 * y + 'px )');
   $('#tailSmall1').css('left', 'calc( -10% + ' + 0.01 * x + 'px)');
 
   // 背景小尾巴(右)
-
   $('#tailSmall2').css('top', 'calc( -20vh + ' + 0.01 * y + 'px )');
   $('#tailSmall2').css('right', 'calc( -15% + ' + -0.01 * x + 'px)');
 
   //馬
-
   $('#horse').css('top', 'calc( 5vh + ' + 0.01 * y + 'px )');
   $('#horse').css('right', 'calc( 10% + ' + -0.01 * x + 'px)');
 
   //彩虹
-
   $('#rainbow').css('top', 'calc( 5vh + ' + -0.03 * y + 'px )');
   $('#rainbow').css('right', 'calc( 0% + ' + 0.03 * x + 'px)');
 
   //三角錐(橘)
-
   $('#cornO').css('top', 'calc( 68vh + ' + -0.03 * y + 'px )');
   $('#cornO').css('right', 'calc( 5% + ' + 0.03 * x + 'px)');
 
