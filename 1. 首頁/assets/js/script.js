54e028e4de54ef178497391d087bf1c16e358849
CL Wu <CharlieWu500@gmail.com>
2022-06-16 16:41:21 +0800

    push new css

---
+++ 1. 首頁/assets/js/script.js
@@ -0,0 +1,11 @@
+// scroll
+function show(event) {
+  let y = event.screenY;
+  console.log(y);
+  const latestY = $('.latest').screenY;
+  console.log(latestY);
+  // whatsOnY
+  // aboutFilmFesY
+  // manualY
+  // getItY
+}
