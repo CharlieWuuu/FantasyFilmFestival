13ee46b42aac9dd0eea4c492917fea81f91660a6
CL Wu <CharlieWu500@gmail.com>
2022-06-20 11:35:25 +0800

    change bg css

--- 1. 首頁/assets/js/script.js
+++ 1. 首頁/assets/js/script.js
@@ -1,11 +1,22 @@
-// scroll
-function show(event) {
-  let y = event.screenY;
-  console.log(y);
-  const latestY = $('.latest').screenY;
-  console.log(latestY);
-  // whatsOnY
-  // aboutFilmFesY
-  // manualY
-  // getItY
-}
+/* FadeIn Scroll */
+$(document).ready(function () {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function () {
+    /* Check the location of each desired element */
+    $('article').each(function (i) {
+      var bottom_of_object =
+        $(this).position().top + $(this).outerHeight() * 0.2;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: '1' }, 900);
+      }
+    });
+  });
+});
+
+$(this).scroll(function () {
+  const Y = scrollY;
+  $('.background').css('top', 0 + 0.04 * Y + 'vh');
+});
